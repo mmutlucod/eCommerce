148264618754c9572876ecd239749fd8374c21bb
UmutCan <50389219+umutcann34@users.noreply.github.com>
2024-03-17 17:14:32 +0300

    update routes

--- eCommerce-backend/routes/seller.js
+++ eCommerce-backend/routes/seller.js
@@ -9,14 +9,14 @@ const sellerProduct = require('../models/sellerProduct');
 
 router.post('/login', sellerController.login);
 router.post('/register', sellerController.register);
-router.get('/listSellers', sellerController.listSellers);
+router.get('/listSellers', authMiddleware, sellerController.listSellers);
 
 router.get('/products', authMiddleware, sellerController.getProducts);
 router.get('/products/:id', authMiddleware, sellerController.getProductDetailsById);
 router.post('/create-seller-product', authMiddleware, sellerController.createProduct);
 router.put('/products/:id', authMiddleware, sellerController.updateProduct);
-router.put('/activateProduct/:id', sellerController.activateProduct);
-router.put('/deactivateProduct/:id', sellerController.deactivateProduct);
+router.put('/activateProduct/:id', authMiddleware, sellerController.activateProduct);
+router.put('/deactivateProduct/:id', authMiddleware, sellerController.deactivateProduct);
 
 router.get('/allBrands', authMiddleware, sellerController.getAllBrands);
 router.get('/brands', authMiddleware, sellerController.getSellerBrands);
@@ -27,11 +27,11 @@ router.put('/brands/:id', authMiddleware, sellerController.updateBrand);
 
 
 router.get('/categories', authMiddleware, sellerController.getAllCategories);
-router.get('/searchCategories', sellerController.getAllCategoriesWithSearch);
-router.patch('/update-order-status/:id', sellerController.updateOrderStatus);
+router.get('/searchCategories', authMiddleware, sellerController.getAllCategoriesWithSearch);
+router.patch('/update-order-status/:id', authMiddleware, sellerController.updateOrderStatus);
 router.get('/orders', authMiddleware, sellerController.getSellerOrders);
-router.post('/update-shipping-code/:id', sellerController.updateShippingCodeOrderItem);
-router.post('/cancel-order-item', sellerController.cancelOrderItemQuantity);
+router.post('/update-shipping-code/:id', authMiddleware, sellerController.updateShippingCodeOrderItem);
+router.post('/cancel-order-item', authMiddleware, sellerController.cancelOrderItemQuantity);
 
 
 module.exports = router;
