797aa57ee037ae5548a10d9216e0c75450e3fd20
mmutlucod <110405404+mmutlucod@users.noreply.github.com>
2024-05-15 16:45:54 +0300

    summary

--- ecommerce/src/user/PaymentPage.js
+++ ecommerce/src/user/PaymentPage.js
@@ -16,23 +16,27 @@ import {
     Box,
     TextField,
     MenuItem,
-    CssBaseline
+    CssBaseline,
+    Checkbox,
+    Alert
 } from '@mui/material';
 import UserNavbar from '../components/UserNavbar';
 import UserFooter from '../components/UserFooter';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AddIcon from '@mui/icons-material/Add';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import Card from '../components/Card/Card'; // Card bileşeni
-import CForm from '../components/Form/Form'; // CForm bileşeni
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Card from '../components/Card/Card';
+import CForm from '../components/Form/Form';
 
 const theme = createTheme({
     palette: {
         primary: {
-            main: '#9370DB', // Açık mor renk
+            main: '#9370DB',
         },
         secondary: {
-            main: '#FFA500', // Turuncu renk
+            main: '#FFA500',
         },
     },
 });
@@ -55,11 +59,16 @@ const PaymentPage = () => {
     const [selectedAddress, setSelectedAddress] = useState('');
     const [isModalOpen, setModalOpen] = useState(false);
     const [newAddress, setNewAddress] = useState({ address_line: '', city: '', state: '', postal_code: '' });
+    const [termsAccepted, setTermsAccepted] = useState(false);
+    const [contractOpen, setContractOpen] = useState(false);
+    const [infoAccepted, setInfoAccepted] = useState(false);
+    const [infoOpen, setInfoOpen] = useState(false);
+    const [formValid, setFormValid] = useState(false);
+    const [alertMessage, setAlertMessage] = useState('');
 
-    // Card form state ve handler fonksiyonları
     const [state, setState] = useState({
-        cardNumber: '#### #### #### ####',
-        cardHolder: 'FULL NAME',
+        cardNumber: '',
+        cardHolder: '',
         cardMonth: '',
         cardYear: '',
         cardCvv: '',
@@ -75,7 +84,19 @@ const PaymentPage = () => {
         }));
     }, []);
 
-    // Form input referansları
+    const validateForm = () => {
+        const { cardNumber, cardHolder, cardMonth, cardYear, cardCvv } = state;
+        if (cardNumber && cardHolder && cardMonth && cardYear && cardCvv) {
+            setFormValid(true);
+        } else {
+            setFormValid(false);
+        }
+    };
+
+    useEffect(() => {
+        validateForm();
+    }, [state]);
+
     let formFieldsRefObj = {
         cardNumber: useRef(),
         cardHolder: useRef(),
@@ -87,7 +108,6 @@ const PaymentPage = () => {
         formFieldsRefObj[key].current.focus();
     }, []);
 
-    // Kart element referansları
     let cardElementsRef = {
         cardNumber: useRef(),
         cardHolder: useRef(),
@@ -118,35 +138,142 @@ const PaymentPage = () => {
         fetchAddresses();
     }, []);
 
-    const handleNext = () => setActiveStep(prev => prev + 1);
+    const handleNext = () => {
+        if (activeStep === steps.length - 1 && !formValid) {
+            setAlertMessage('Lütfen eksik alanları doldurunuz.');
+        } else {
+            setAlertMessage('');
+            setActiveStep(prev => prev + 1);
+        }
+    };
+
     const handleBack = () => setActiveStep(prev => prev - 1);
     const handleRadioChange = event => setSelectedAddress(event.target.value);
     const handleModalOpen = () => setModalOpen(true);
     const handleModalClose = () => setModalOpen(false);
+    const handleContractOpen = () => setContractOpen(true);
+    const handleContractClose = () => setContractOpen(false);
+    const handleInfoOpen = () => setInfoOpen(true);
+    const handleInfoClose = () => setInfoOpen(false);
     const handleInputChange = event => setNewAddress(prev => ({ ...prev, [event.target.name]: event.target.value }));
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+    const handleTermsChange = event => setTermsAccepted(event.target.checked);
+    const handleInfoChange = event => setInfoAccepted(event.target.checked);
+    const handleSubmit = async () => {
+        if (!formValid || !termsAccepted || !infoAccepted) {
+            setAlertMessage('Lütfen tüm alanları doldurun ve koşulları kabul edin.');
+            return;
+        }
+
         try {
-            await api.post('/user/create-address', newAddress);
-            setModalOpen(false);
-            // Adresi ekledikten sonra adresleri tekrar yükleyin
-            const response = await api.get('user/addresses');
-            setAddresses(response.data.addresses || []);
-            setSelectedAddress(response.data.addresses[response.data.addresses.length - 1].address_id.toString());
+            console.log(selectedAddress)
+            const orderData = {
+                address_id: 1,
+                
+                // Diğer sipariş detayları burada
+            };
+
+            await api.post('/user/create-order', orderData);
+            setAlertMessage('Siparişiniz başarıyla oluşturuldu!');
         } catch (error) {
-            console.error('Error adding address:', error);
+            console.error('Error creating order:', error);
+            setAlertMessage('Sipariş oluşturulurken bir hata oluştu.');
         }
     };
+
+    const renderPaymentDetails = () => (
+        <Grid container spacing={2}>
+            <Grid item xs={12} md={8}>
+                <Box sx={{ padding: 2, border: '1px solid #ddd', borderRadius: 2, marginTop: 5 }}>
+                    <Typography variant="h5" sx={{ mb: 2 }}>Ödeme Bilgileri</Typography>
+                    <CForm
+                        cardMonth={state.cardMonth}
+                        cardYear={state.cardYear}
+                        onUpdateState={updateStateValues}
+                        cardNumberRef={formFieldsRefObj.cardNumber}
+                        cardHolderRef={formFieldsRefObj.cardHolder}
+                        cardDateRef={formFieldsRefObj.cardDate}
+                        onCardInputFocus={onCardFormInputFocus}
+                        onCardInputBlur={onCardInputBlur}
+                        cardCvv={formFieldsRefObj.cardCvv}
+                    >
+                        <Card
+                            cardNumber={state.cardNumber}
+                            cardHolder={state.cardHolder}
+                            cardMonth={state.cardMonth}
+                            cardYear={state.cardYear}
+                            cardCvv={state.cardCvv}
+                            isCardFlipped={state.isCardFlipped}
+                            currentFocusedElm={currentFocusedElm}
+                            onCardElementClick={focusFormFieldByKey}
+                            cardNumberRef={cardElementsRef.cardNumber}
+                            cardHolderRef={cardElementsRef.cardHolder}
+                            cardDateRef={cardElementsRef.cardDate}
+                        />
+                    </CForm>
+                    {alertMessage && <Alert severity="error">{alertMessage}</Alert>}
+                </Box>
+            </Grid>
+            <Grid item xs={12} md={4}>
+                <Box sx={{ padding: 2, border: '1px solid #ddd', borderRadius: 2, marginTop: 5 }}>
+                    <Typography variant="h6" gutterBottom>Kullanıcı Sözleşmesi</Typography>
+                    <FormControlLabel
+                        control={<Checkbox checked={termsAccepted} onChange={handleTermsChange} />}
+                        label={
+                            <span>
+                                Kullanım koşullarını kabul ediyorum (
+                                <Typography component="span" sx={{ textDecoration: 'underline', cursor: 'pointer' }} onClick={handleContractOpen}>
+                                    Mesafeli Satış Sözleşmesi
+                                </Typography>
+                                )
+                            </span>
+                        }
+                    />
+                    <FormControlLabel
+                        control={<Checkbox checked={infoAccepted} onChange={handleInfoChange} />}
+                        label={
+                            <span>
+                                Ön bilgilendirme koşullarını kabul ediyorum (
+                                <Typography component="span" sx={{ textDecoration: 'underline', cursor: 'pointer' }} onClick={handleInfoOpen}>
+                                    Ön Bilgilendirme Koşulları
+                                </Typography>
+                                )
+                            </span>
+                        }
+                    />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        disabled={!termsAccepted || !infoAccepted || !formValid}
+                        onClick={handleSubmit}
+                        sx={{
+                            bgcolor: '#f27a1a',
+                            color: '#fff',
+                            padding: '13px 40px',
+                            borderRadius: '5px',
+                            fontSize: '1rem',
+                            marginTop: 2,
+                            '&:hover': {
+                                bgcolor: '#f27a1a',
+                                transform: 'none'
+                            }
+                        }}
+                    >
+                        Siparişi Tamamla
+                    </Button>
+                </Box>
+            </Grid>
+        </Grid>
+    );
 
     const renderContent = () => {
         if (activeStep === 0) {
             return (
-                <Box sx={{ padding: 2, border: '1px solid #ddd', borderRadius: 2, marginTop: 5 }}>
+                <Box sx={{ padding: 2, border: '1px solid #ddd', borderRadius: 2, marginTop: 5, marginX: 'auto', maxWidth: '1200px' }}>
                     <Typography variant="h5" sx={{ mb: 2 }}>Adresler</Typography>
                     <Grid container spacing={2} justifyContent="center">
                         <Grid item xs={12} sm={6} md={4}>
                             <MuiCard variant="outlined" sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-                                <Button variant="outlined" color="primary" startIcon={<AddIcon />} onClick={handleModalOpen} sx={{ width: '100%', padding: 2 }}>
+                                <Button variant="outlined" color="primary" startIcon={<AddIcon />} onClick={handleModalOpen} sx={{ width: '100%', height: '100%', padding: 2 }}>
                                     Adres Ekle
                                 </Button>
                             </MuiCard>
@@ -170,35 +297,7 @@ const PaymentPage = () => {
                 </Box>
             );
         } else {
-            return (
-                <Box sx={{ padding: 2, border: '1px solid #ddd', borderRadius: 2, marginTop: 5 }}>
-                    <Typography variant="h5" sx={{ mb: 2 }}>Ödeme Bilgileri</Typography>
-                    <CForm
-                        cardMonth={state.cardMonth}
-                        cardYear={state.cardYear}
-                        onUpdateState={updateStateValues}
-                        cardNumberRef={formFieldsRefObj.cardNumber}
-                        cardHolderRef={formFieldsRefObj.cardHolder}
-                        cardDateRef={formFieldsRefObj.cardDate}
-                        onCardInputFocus={onCardFormInputFocus}
-                        onCardInputBlur={onCardInputBlur}
-                    >
-                        <Card
-                            cardNumber={state.cardNumber}
-                            cardHolder={state.cardHolder}
-                            cardMonth={state.cardMonth}
-                            cardYear={state.cardYear}
-                            cardCvv={state.cardCvv}
-                            isCardFlipped={state.isCardFlipped}
-                            currentFocusedElm={currentFocusedElm}
-                            onCardElementClick={focusFormFieldByKey}
-                            cardNumberRef={cardElementsRef.cardNumber}
-                            cardHolderRef={cardElementsRef.cardHolder}
-                            cardDateRef={cardElementsRef.cardDate}
-                        />
-                    </CForm>
-                </Box>
-            );
+            return renderPaymentDetails();
         }
     };
 
@@ -206,24 +305,60 @@ const PaymentPage = () => {
         <ThemeProvider theme={theme}>
             <CssBaseline />
             <UserNavbar />
-            <Container maxWidth="lg" disableGutters sx={{ px: 4 }}>
-                <Stepper activeStep={activeStep} alternativeLabel sx={{ width: '100%', bgcolor: theme.palette.primary.main }}>
-                    {steps.map((label, index) => (
-                        <Step key={label}>
-                            <StepLabel StepIconComponent={StepIconComponent}>{label}</StepLabel>
-                        </Step>
-                    ))}
-                </Stepper>
+            <Container maxWidth={false} disableGutters>
+                <Box sx={{ width: '100%', bgcolor: theme.palette.primary.main, padding: 0 }}>
+                    <Stepper activeStep={activeStep} alternativeLabel sx={{ width: '100%', padding: '20px 0' }}>
+                        {steps.map((label, index) => (
+                            <Step key={label}>
+                                <StepLabel StepIconComponent={StepIconComponent}>{label}</StepLabel>
+                            </Step>
+                        ))}
+                    </Stepper>
+                </Box>
                 {renderContent()}
-                <Grid container spacing={2} sx={{ mt: 2 }}>
-                    <Grid item>
-                        <Button disabled={activeStep === 0} onClick={handleBack} sx={{ bgcolor: theme.palette.secondary.main }}>Geri</Button>
-                    </Grid>
-                    <Grid item>
-                        <Button variant="contained" color="primary" onClick={handleNext} sx={{ bgcolor: theme.palette.secondary.main }}>
-                            {activeStep === steps.length - 1 ? 'Bitir' : 'İleri'}
-                        </Button>
-                    </Grid>
+                <Grid container spacing={2} justifyContent="center" sx={{ mt: 2 }}>
+                    {activeStep !== 0 && (
+                        <Grid item>
+                            <Button
+                                onClick={handleBack}
+                                sx={{
+                                    bgcolor: '#f27a1a',
+                                    color: '#fff',
+                                    padding: '13px 40px',
+                                    borderRadius: '5px',
+                                    fontSize: '1rem',
+                                    '&:hover': {
+                                        bgcolor: '#f27a1a',
+                                        transform: 'none'
+                                    }
+                                }}
+                            >
+                                <ArrowBackIcon /> Geri
+                            </Button>
+                        </Grid>
+                    )}
+                    {activeStep < steps.length - 1 && (
+                        <Grid item>
+                            <Button
+                                onClick={handleNext}
+                                sx={{
+                                    bgcolor: '#f27a1a',
+                                    color: '#fff',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    padding: '13px 40px',
+                                    borderRadius: '5px',
+                                    fontSize: '1rem',
+                                    '&:hover': {
+                                        bgcolor: '#f27a1a',
+                                        transform: 'none'
+                                    }
+                                }}
+                            >
+                                İleri <ArrowForwardIcon />
+                            </Button>
+                        </Grid>
+                    )}
                 </Grid>
             </Container>
             <Modal open={isModalOpen} onClose={handleModalClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
@@ -246,11 +381,29 @@ const PaymentPage = () => {
                                 <TextField fullWidth label="Posta Kodu" name="postal_code" value={newAddress.postal_code} onChange={handleInputChange} />
                             </Grid>
                             <Grid item xs={12}>
-                                <Button type="submit" variant="contained" color="primary" fullWidth>Adres Ekle</Button>
+                                <Button type="submit" variant="contained" color="primary" fullWidth sx={{ bgcolor: '#f27a1a', color: '#fff' }}>Adres Ekle</Button>
                             </Grid>
                         </Grid>
                     </form>
                     <Button onClick={handleModalClose}>Kapat</Button>
+                </Box>
+            </Modal>
+            <Modal open={contractOpen} onClose={handleContractClose} aria-labelledby="contract-modal-title" aria-describedby="contract-modal-description">
+                <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', boxShadow: 24, p: 4, borderRadius: 2 }}>
+                    <Typography id="contract-modal-title" variant="h6" component="h2">Mesafeli Satış Sözleşmesi</Typography>
+                    <Typography id="contract-modal-description" sx={{ mt: 2 }}>
+                        Mesafeli Satış Sözleşmesi maddeleri burada yer alacak...
+                    </Typography>
+                    <Button onClick={handleContractClose} sx={{ mt: 2 }}>Kapat</Button>
+                </Box>
+            </Modal>
+            <Modal open={infoOpen} onClose={handleInfoClose} aria-labelledby="info-modal-title" aria-describedby="info-modal-description">
+                <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', boxShadow: 24, p: 4, borderRadius: 2 }}>
+                    <Typography id="info-modal-title" variant="h6" component="h2">Ön Bilgilendirme Koşulları</Typography>
+                    <Typography id="info-modal-description" sx={{ mt: 2 }}>
+                        Ön Bilgilendirme Koşulları maddeleri burada yer alacak...
+                    </Typography>
+                    <Button onClick={handleInfoClose} sx={{ mt: 2 }}>Kapat</Button>
                 </Box>
             </Modal>
             <UserFooter />
