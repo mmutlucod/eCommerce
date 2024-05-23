9e35a6061bcc6f65b3e88cf06f7d40307da3ac8e
UmutCan <50389219+umutcann34@users.noreply.github.com>
2024-05-23 15:55:57 +0300

    update

--- eCommerce-backend/models/orderStatus.js
+++ eCommerce-backend/models/orderStatus.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = require('../utility/db');
 
 const orderStatus = sequelize.define('orderStatus', {
-    status_id: {
+    order_status_id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
         allowNull: false,
@@ -15,12 +15,12 @@ const orderStatus = sequelize.define('orderStatus', {
 orderStatus.afterSync(async () => {
     // İstenen başlangıç durumlarını tanımla
     const statuses = [
-        { status_id: 1, status_name: 'Sipariş Alındı' },
-        { status_id: 2, status_name: 'Sipariş Hazırlanıyor' },
-        { status_id: 3, status_name: 'Sipariş Kargoya Verildi' },
-        { status_id: 4, status_name: 'Sipariş Teslim Edildi' },
-        { status_id: 5, status_name: 'Siparişiniz İptal Edildi' },
-        { status_id: 6, status_name: 'Siparişiniz İade Edildi' }
+        { order_status_id: 1, status_name: 'Sipariş Alındı' },
+        { order_status_id: 2, status_name: 'Sipariş Hazırlanıyor' },
+        { order_status_id: 3, status_name: 'Sipariş Kargoya Verildi' },
+        { order_status_id: 4, status_name: 'Sipariş Teslim Edildi' },
+        { order_status_id: 5, status_name: 'Siparişiniz İptal Edildi' },
+        { order_status_id: 6, status_name: 'Siparişiniz İade Edildi' }
     ];
 
     // Bulk create ile durumları veritabanına ekle
