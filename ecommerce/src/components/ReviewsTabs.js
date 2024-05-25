aface99c1f8fb60138e27ef0d21e07e4ce3245e5
mmutlucod <110405404+mmutlucod@users.noreply.github.com>
2024-05-25 13:03:07 +0300

    cd

--- ecommerce/src/components/ReviewsTabs.js
+++ ecommerce/src/components/ReviewsTabs.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Typography, Rating, Grid, Paper, List, Card, CardHeader, Avatar, CardContent, IconButton, Link, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Box, Typography, Rating, Grid, Paper, List, Card, CardHeader, Avatar, CardContent, Button, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import api from '../api/api';
 
 function formatUserName(name, isPublic) {
@@ -56,29 +55,29 @@ function ReviewsTab({ productId }) {
 
   const filteredReviews = reviews.filter(review => review.rating.toString().includes(selectedRating));
 
-  if (filteredReviews.length > 0) {
-    return (
-      <Box sx={{ p: 2 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={4}>
-            <Paper elevation={3} sx={{ p: 2 }}>
-              <Typography variant="h6" sx={{ mb: 2 }}>Genel Puan</Typography>
-              {Object.keys(ratingSummary).sort((a, b) => b - a).map((key) => (
-                <Box key={key} sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
-                  <Rating value={parseInt(key)} readOnly />
-                  <Typography sx={{ ml: 1 }}>{ratingSummary[key]} değerlendirme</Typography>
-                </Box>
-              ))}
-            </Paper>
-          </Grid>
-          <Grid item xs={8}>
-            <FormControl fullWidth>
+  return (
+    <Box sx={{ p: 2 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Paper elevation={3} sx={{ p: 2 }}>
+            <Typography variant="h6" sx={{ mb: 2 }}>Genel Puan</Typography>
+            {Object.keys(ratingSummary).sort((a, b) => b - a).map((key) => (
+              <Box key={key} sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
+                <Rating value={parseInt(key)} readOnly />
+                <Typography sx={{ ml: 1 }}>{ratingSummary[key]} değerlendirme</Typography>
+              </Box>
+            ))}
+          </Paper>
+        </Grid>
+        <Grid item xs={8}>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <FormControl sx={{ width:'70%' }}>
               <InputLabel id="rating-select-label">Filtrele</InputLabel>
               <Select
                 labelId="rating-select-label"
                 id="rating-select"
                 value={selectedRating}
-                label="Filter by Rating"
+                label="Filtrele"
                 onChange={handleRatingChange}
               >
                 <MenuItem value="">
@@ -89,45 +88,56 @@ function ReviewsTab({ productId }) {
                 ))}
               </Select>
             </FormControl>
-            <List sx={{ width: '100%', bgcolor: 'background.paper', mt: 2 }}>
-              {filteredReviews.map((review) => (
-                <Card key={review.id} variant="outlined" sx={{ mb: 2, bgcolor: 'grey.200' }}>
-                  <CardHeader
-                    avatar={
-                      <Avatar sx={{ bgcolor: 'secondary.main' }}>
-                        {review.user.name[0]}{review.user.surname[0]}
-                      </Avatar>
-                    }
-                    title={`${review.user.name} ${review.user.surname}`}
-                    subheader={new Date(review.date).toLocaleString('tr-TR', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' })}
-                    titleTypographyProps={{ variant: 'subtitle2' }}
-                    subheaderTypographyProps={{ variant: 'caption' }}
-                  />
-                  <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                      {review.content}
-                    </Typography>
-                    <Rating value={review.rating} readOnly sx={{ mt: 1 }} />
-                    <Link href="#" underline="always" sx={{ display: 'block', mt: 1, fontSize: '0.75rem' }}>
-                      Bildir
-                    </Link>
-                  </CardContent>
-                </Card>
-              ))}
-            </List>
-          </Grid>
+            <Button
+              variant="contained"
+              color="secondary"
+              sx={{
+                height:56,
+                color: 'white',
+                bgcolor: '#f27a1a', // Buton arka plan rengi turuncu
+                '&:hover': {
+                  bgcolor: '#f27a1a' // Hover durumu için renk sabit turuncu
+                }
+              }}
+              onClick={() => alert('Değerlendirme ekleme butonuna tıklandı')}
+            >
+              Değerlendirme Ekle
+            </Button>
+          </Box>
+          <List sx={{ width: '100%', bgcolor: 'background.paper', mt: 2 }}>
+            {filteredReviews.length > 0 ? filteredReviews.map((review) => (
+              <Card key={review.id} variant="outlined" sx={{ mb: 2, bgcolor: 'grey.200' }}>
+                <CardHeader
+                  avatar={
+                    <Avatar sx={{ bgcolor: 'secondary.main' }}>
+                      {review.user.name[0]}{review.user.surname[0]}
+                    </Avatar>
+                  }
+                  title={`${review.user.name} ${review.user.surname}`}
+                  subheader={new Date(review.date).toLocaleString('tr-TR', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' })}
+                  titleTypographyProps={{ variant: 'subtitle2' }}
+                  subheaderTypographyProps={{ variant: 'caption' }}
+                />
+                <CardContent>
+                  <Typography variant="body2" color="text.secondary">
+                    {review.content}
+                  </Typography>
+                  <Rating value={review.rating} readOnly sx={{ mt: 1 }} />
+                  <Button variant="text" size="small" sx={{ mt: 1 }}>
+                    Bildir
+                  </Button>
+                </CardContent>
+              </Card>
+            )) : (
+              <Typography variant="body2" sx={{ mt: 5, color: 'gray' }}>
+                Bu ürüne henüz yorum yapılmadı.
+              </Typography>
+            )}
+          </List>
         </Grid>
-      </Box>
-    );
-  }
-  else {
-    return (
-      <Typography variant="body2" sx={{ mt: 5, color: 'gray' }}>
-        Bu ürüne henüz yorum yapılmadı.
-      </Typography>
-    )
-  }
-
+      </Grid>
+    </Box>
+  );
 }
 
 export default ReviewsTab;
